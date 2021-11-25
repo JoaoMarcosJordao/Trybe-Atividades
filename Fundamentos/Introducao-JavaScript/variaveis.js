aaabd273b64a2b4d2a5a71935929e0b41bb06eed
JoaoMarcos <joaomarc97@gmail.com>
2021-11-25 16:05:17 -0300

    Atualizando

--- Fundamentos/Introducao-JavaScript/variaveis.js
+++
@@ -1,5 +0,0 @@
-const myName = "Joao";
-const birthCity = "Rio de Janeiro";
-let birthYear = 1997;
-
-console.log("Meu nome é " + myName + " nascido na cidade " + birthCity + " no ano de "+ birthYear+".");
